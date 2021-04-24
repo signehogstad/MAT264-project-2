91a72ad05131bc183d9d61d9c5d245ed91c62150
Tooffeee <79586170+Tooffeee@users.noreply.github.com>
2021-04-24 15:46:05 +0200

    Update main.m

--- real_shit/main.m
+++ real_shit/main.m
@@ -37,6 +37,27 @@ hold on
 
 plot(x,u_eksakt(x),'k','LineWidth',1.2,'LineStyle','--')
 
+% Runge error, possibly terribly wrong rip.
+% NB: Only works for Nh1 = n * Nh2, where n is an integer.
+Nh1 = 200; Nh2 = 50;
 
+[xh1,uh1] = create_for_runge(X,Nh1,f);
+[xh2,uh2] = create_for_runge(X,Nh2,f);
+
+[runge_error_bound, uh2_new] = Runge_error(uh1,uh2);
+runge_error_bound
+real_error_L2 = norm(u_eksakt(xh1) - uh1,2)
+
+% Make a longer xh2 (xh2_new) to match uh2_new
+xh2_new = make_longer(xh2,xh1);
+
+% Plotting xh2/uh2 and xh2_new/uh2_new for comparison just for lolz
+plot(xh2_new,uh2_new,'b','LineWidth',1.5)
+hold on
+
+plot(xh2,uh2,'k','LineStyle','--','LineWidth',1.5)
+hold on
+
+plot(xh1,uh2_new,'m','LineWidth',1.5)
 
 
