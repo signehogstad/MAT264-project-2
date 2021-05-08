71563ecc903ec330915ddefadc20efa848baccc8
Tooffeee <79586170+Tooffeee@users.noreply.github.com>
2021-05-08 15:59:18 +0200

    Add files via upload

--- real_shit/2D/PoissonEqExample.m
+++ real_shit/2D/PoissonEqExample.m
@@ -1,21 +1,25 @@
 % Poisson equation: -Div(k(x,y)Grad(u)) = f
 hold off;
 clear; clc;
+format short
 
 %Diffusivity
-k = @(x,y) 1+(x-1).*(y-1);
-% k = @(x,y) ones(length(x),length(y));
-% k = @(x,y) 1/x+3.*(x-1);
+% k = @(x,y) 1+(x-1).*(y-1);
+k = @(x,y) ones(length(x),length(y));
+
 
 % Exact solution
 u_fabricated = @(x,y) sin(2*pi*x).*sin(2*pi*y);
+% u_fabricated = @(x,y) x.^2.*y.^2-x.^2.*y-x.*y.^2+x.*y;
 
 %RHS function
 f = @(x,y) 8*pi*pi*sin(2*pi*x).*sin(2*pi*y).*k(x,y)-(2*pi*cos(2*x*pi).*sin(2*pi*y).*(y-1)+2*pi*sin(2*pi*x).*cos(2*pi*y).*(x-1));
+% f = @(x,y) -2*y^2+2*y-2*x^2+2*x;
+fi =  @(x,y) (8*pi*pi*sin(2*pi*x).*sin(2*pi*y).*k(x,y)-(2*pi*cos(2*x*pi).*sin(2*pi*y).*(y-1)+2*pi*sin(2*pi*x).*cos(2*pi*y).*(x-1)))^2;
 
 %Determine number of cells in each direction
-nx = 5;
-ny = 5;
+nx = 40;
+ny = 40;
 dx = 1/(nx-1);
 dy = 1/(ny-1);
 
@@ -68,7 +72,7 @@ colorbar()
 
 q = -K.*G*u;
 
-[avg_pot,int_pot_mat] = avg_pot_int_pot(nx,ny,U,X,Y);
+[avg_pot,int_pot_mat,a] = avg_pot_int_pot(nx,ny,U,X,Y);
 
 cells;
 edges;
@@ -77,6 +81,33 @@ surf(X,Y,int_pot_mat)
 axis([0 1 0 1])
 xlabel('x')
 ylabel('y')
-title('Average potential on corners')
+title('Interpolated potential yay')
 
 colorbar()
+
+maj_int = 0;
+int_val = 0;
+for i = 1:nx*ny
+    [coord_vals,flux_vals] = get_flux_and_coord(cells,edges,q,a,i,f);
+    
+    x0 = coord_vals(1,1); x1 = coord_vals(1,2);
+    y0 = coord_vals(2,1); y1 = coord_vals(2,2);
+    
+    %IDK? Får for syk konvergens av dette. Men tror det blir feil å ikke ha
+    %det med, såååå idk....
+    dy = y1-y0; dx = x1-x0;
+    
+    rx0 = flux_vals(1,1); rx1 = flux_vals(1,2);
+    ry0 = flux_vals(2,1); ry1 = flux_vals(2,2);
+    
+    maj_int = maj_int + energy_error_integral(x0,x1,y0,y1,rx0,rx1,ry0,ry1,a);
+    
+    fi = @(x,y) (8*pi*pi*sin(2*pi*x).*sin(2*pi*y).*k(x,y)-(2*pi*cos(2*x*pi).*sin(2*pi*y).*(y-1)+2*pi*sin(2*pi*x).*cos(2*pi*y).*(x-1)) - ((rx1-rx0)/dx + (ry1-ry0)/dy))^2;
+    
+    int_val = int_val + midpoint_integral_f(x0,x1,y0,y1,fi);
+end
+maj_int = sqrt(maj_int);
+int_val = 1/(pi*sqrt(2))*sqrt(int_val)
+
+majorant = maj_int + int_val;
+colormap winter
---
+++ real_shit/2D/get_flux_and_coord.m
@@ -0,0 +1,90 @@
+function [coord_vals,flux_vals] = get_flux_and_coord(cells,edges,q,a,i,f)
+
+w = 6; n = 5; e = 4; s = 3;
+
+% Cell_info looks like: [x y south east north west]
+cell_info = cells(i,:);
+W = cell_info(w); N = cell_info(n); E = cell_info(e); S = cell_info(s);
+% Getting x0, y0, x1, y1 by fetching edges(s,:), edges(e,:) and so on.
+% Erlend har vært supernice and given us all these matriser. edges(s,:)
+% will be information abouth the southern end of the cell. It will
+% contain a "center"-value as x-coordinate, and will be y0 as
+% y-coordinate.
+
+% Corners:
+if W == 0 && S == 0
+    x0 = 0; x1v = edges(E,:); x1 = x1v(1); 
+    y0 = 0; y1v = edges(N,:); y1 = y1v(2); 
+    rx0 = 0; rx1 = 0;
+    ry0 = 0; ry1 = 0;
+elseif S == 0 && E == 0
+    x0v = edges(W,:); x0 = x0v(1); x1 = 1;
+    y0 = 0; y1v = edges(N,:); y1 = y1v(2);
+    rx0 = 0; rx1 = 0;
+    ry0 = 0; ry1 = 0;
+elseif E == 0 && N == 0
+    x0v = edges(W,:); x0 = x0v(1); x1 = 1;
+    y0v = edges(S,:); y0 = y0v(2); y1 = 1;
+    rx0 = 0; rx1 = 0;
+    ry0 = 0; ry1 = 0;
+elseif W == 0 && N == 0
+    x0 = 0; x1v = edges(E,:); x1 = x1v(1);
+    y0v = edges(S,:); y0 = y0v(2); y1 = 1;
+    rx0 = 0; rx1 = 0;
+    ry0 = 0; ry1 = 0;
+
+% All edges:
+
+% WEST
+elseif W == 0
+    x0 = 0; x1v = edges(E,:); x1 = x1v(1); 
+    y0v = edges(S,:); y0 = y0v(2); y1v = edges(N,:); y1 = y1v(2); 
+    
+    dy = y1-y0;
+    
+    rx1 = q(E); rx0 = rx1 - 1/dy*midpoint_integral_f(x0,x1,y0,y1,f); 
+    ry0 = 0; ry1 = 0;
+    
+% SOUTH
+elseif S == 0
+    x0v = edges(W,:); x0 = x0v(1); x1v = edges(E,:); x1 = x1v(1);
+    y0 = 0; y1v = edges(N,:); y1 = y1v(2);
+    
+    dx = x1-x0;
+    
+    rx0 = 0; rx1 = 0;
+    ry1 = q(N); ry0 = ry1 - 1/dx*midpoint_integral_f(x0,x1,y0,y1,f); 
+    
+% EAST
+elseif E == 0
+    x0v = edges(W,:); x0 = x0v(1); x1 = 1;
+    y0v = edges(S,:); y0 = y0v(2); y1v = edges(N,:); y1 = y1v(2);
+    
+    dy = y1-y0;
+    
+    rx0 = q(W); rx1 = rx0 + 1/dy*midpoint_integral_f(x0,x1,y0,y1,f);
+    ry0 = 0; ry1 = 0;
+    
+% NORTH
+elseif N == 0
+    x0v = edges(W,:); x0 = x0v(1); x1v = edges(E,:); x1 = x1v(1);
+    y0v = edges(S,:); y0 = y0v(2); y1 = 1;
+    
+    dx = x1-x0;
+    
+    rx0 = 0; rx1 = 0;
+    ry0 = q(S); ry1 = ry0 + 1/dx*midpoint_integral_f(x0,x1,y0,y1,f);
+
+% All other cells:
+else
+    x0v = edges(W,:); x0 = x0v(1); x1v = edges(E,:); x1 = x1v(1);
+    y0v = edges(S,:); y0 = y0v(2); y1v = edges(N,:); y1 = y1v(2);
+    rx0 = q(W); rx1 = q(E);
+    ry0 = q(S); ry1 = q(N);
+end
+
+
+coord_vals = [x0 x1; y0 y1;];
+flux_vals = [rx0 rx1; ry0 ry1;];
+
+end
